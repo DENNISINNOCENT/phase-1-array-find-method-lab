85f022b8f44ea2e6cfb4a9227f45d29564383dbc
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-13 05:22:43 +0300

    commit tree 20e8ca118bbe38f43b7d88d643152e0b03c0c840

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code your solution here
 superbowlWin = record => {
-    const result = record.find( record => record.result === "W" );
+    let result = record.find( record => record.result === "W" );
      return result ? result.year : undefined;
     
   }
